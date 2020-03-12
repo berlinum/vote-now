5fe0c78f13a21a900e7e0a36e372bf6756fd1244
berlinum <maks.filimonov@gmail.com>
2020-03-12 16:43:54 +0100

    Add environment variables

--- src/pages/New.js
+++ src/pages/New.js
@@ -4,13 +4,17 @@ import Form from "../components/Form";
 
 function New() {
   async function handleSubmit(poll) {
-    const response = await fetch("http://localhost:4000/polls", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(poll)
-    });
+    const response = await fetch(
+      process.env.REACT_APP_POLLS_API ||
+        "https://my-json-server.typicode.com/berlinum/vote-now/polls",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(poll)
+      }
+    );
     const createdPool = await response.json();
     alert(`Created a new poll with ID ${createdPool.id}`);
   }
