9a2ac191346c0e2d9437f8af413a87c106da434f
berlinum <maks.filimonov@gmail.com>
2020-03-12 12:35:23 +0100

    Add form submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,64 @@
 import React from "react";
 import "./Form.css";
 import Button from "./Button";
-import App from "../App";
 
 function Form(props) {
+  const [question, setQuestion] = React.useState("");
+  const [answerOne, setAnswerOne] = React.useState("");
+  const [answerTwo, setAnswerTwo] = React.useState("");
+  const [answerThree, setAnswerThree] = React.useState("");
+  const poll = {
+    question: question,
+    answerOne: answerOne,
+    answerTwo: answerTwo,
+    answerThree: answerThree
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onSubmit(poll);
+  }
   return (
-    <form className="form__box">
+    <form className="form__box" onSubmit={handleSubmit}>
       <label className="question__label">{props.question}</label>
       <input
         className="question__input"
         type="text"
         placeholder="Enter question..."
+        value={question}
+        onChange={event => {
+          setQuestion(event.target.value);
+        }}
       ></input>
       <label className="answers__label">{props.answers}</label>
-      <input className="answer__input" type="text" placeholder="Lager"></input>
-      <input className="answer__input" type="text" placeholder="Malt"></input>
-      <input className="answer__input" type="text" placeholder="Ale"></input>
-      <Button buttonName="Create poll" class="button__new" link="/vote" />
+      <input
+        className="answer__input"
+        type="text"
+        placeholder="Lager"
+        value={answerOne}
+        onChange={event => {
+          setAnswerOne(event.target.value);
+        }}
+      ></input>
+      <input
+        className="answer__input"
+        type="text"
+        placeholder="Malt"
+        value={answerTwo}
+        onChange={event => {
+          setAnswerTwo(event.target.value);
+        }}
+      ></input>
+      <input
+        className="answer__input"
+        type="text"
+        placeholder="Ale"
+        value={answerThree}
+        onChange={event => {
+          setAnswerThree(event.target.value);
+        }}
+      ></input>
+      <Button buttonName="Create poll" class="button__new" />
     </form>
   );
 }
--- src/pages/New.js
+++ src/pages/New.js
@@ -3,7 +3,13 @@ import "./New.css";
 import Form from "../components/Form";
 
 function New() {
-  return <Form question="Your question:" answers="Answer options:" />;
+  return (
+    <Form
+      question="Your question:"
+      answers="Answer options:"
+      onSubmit={data => alert(JSON.stringify(data))}
+    />
+  );
 }
 
 export default New;
